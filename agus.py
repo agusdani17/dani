8be307efc3d65dddcf2f6a89993a2e73094c06e8
agusdani17 <117462987+agusdani17@users.noreply.github.com>
2022-11-15 01:15:38 +0800

    Update agus.py

--- agus.py
+++ agus.py
@@ -41,18 +41,18 @@ for xd in range(10000):
 	uaku=(f'{a}{b}.{c} {d}{e}{f}{g}.{h}.{i}.{j} {k}')
 	ugen2.append(uaku)
 
-	aa='Mozilla/5.0 (Linux; selular'
+	aa='Mozilla/5.0 (Windows; Botol;'
 	b=random.choice(['6','7','8','9','10','11','12'])
-	c=' Redmi Build/'
+	c=' Pc-Agus'
 	d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
 	e=random.randrange(1, 999)
 	f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
-	g='/537.36 (KHTML, like Gecko) Ucbrowser/'
+	g='AppleWebKit/537.36 (KHTML, like Gecko) facebook/'
 	h=random.randrange(73,100)
 	i='0'
 	j=random.randrange(4200,4900)
 	k=random.randrange(40,150)
-	l='Safari/537.36 Edge/17.17133'
+	l='facebook Safari/17.008'
 	uaku2=f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'
 	ugen.append(uaku2)
 	
